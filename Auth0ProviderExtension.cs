5bc05d3a3fec03f2664e46f76b7247565935ddf5
Jake Overall <joverall22@gmail.com>
2020-10-22 13:17:06 -0600

    adds simple memory cache to userInfo Fetch

--- Auth0ProviderExtension.cs
+++ Auth0ProviderExtension.cs
@@ -6,12 +6,21 @@ using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
+using System;
+using System.Diagnostics;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace CodeWorks.Auth0Provider
 {
     public static class Auth0ProviderExtension
     {
-        private static List<string> _KeyMap = new List<string>();
+        public static int TTL = 60 * 1000;
+        private static List<string> _KeyMap = new List<string>() { "id" };
+
+        private static MemoryCache _cache = new MemoryCache(new MemoryCacheOptions()
+        {
+            SizeLimit = 1024
+        });
 
         public static void ConfigureKeyMap(List<string> keys)
         {
@@ -20,14 +29,29 @@ namespace CodeWorks.Auth0Provider
                 _KeyMap.Add(key);
             });
         }
-
         public static async Task<T> GetUserInfoAsync<T>(this HttpContext ctx)
         {
             if (!ctx.Request.Headers.ContainsKey("Authorization"))
             {
-                return default(T);
+                throw new Exception("No Bearer Token Provided");
             }
             var bearer = ctx.Request.Headers["Authorization"].FirstOrDefault();
+            dynamic data;
+            if (_cache.TryGetValue(bearer, out data))
+            {
+                return data.ToObject<T>();
+            }
+            data = await FetchFromAuth0(ctx, bearer);
+            T entry = data.ToObject<T>();
+            _cache.Set(bearer, entry, new MemoryCacheEntryOptions()
+            {
+                SlidingExpiration = TimeSpan.FromMilliseconds(TTL)
+            });
+            return entry;
+        }
+
+        private static async Task<dynamic> FetchFromAuth0(HttpContext ctx, string bearer)
+        {
             var client = new HttpClient();
             var requestUrl = ctx.User.FindFirst(c => c.Value.EndsWith("userinfo")).Value;
             var iss = ctx.User.FindFirst(c => c.Type == "iss").Value;
@@ -36,22 +60,22 @@ namespace CodeWorks.Auth0Provider
             client.DefaultRequestHeaders.Add("authorization", bearer);
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             HttpResponseMessage response = await client.GetAsync(requestUrl);
-            if (response.IsSuccessStatusCode)
+            if (!response.IsSuccessStatusCode)
             {
-                var res = await response.Content.ReadAsStringAsync();
-                dynamic data = JObject.Parse(res);
-                foreach (var key in _KeyMap)
-                {
-                    if (data[iss + key] != null)
-                    {
-                        data[key] = data[iss + key];
-                    }
-                }
-                var d = data.ToObject<T>();
-                return d;
+                throw new Exception("Unable to retrieve userInfo from Bearer Token");
             }
-            return default(T);
+            var res = await response.Content.ReadAsStringAsync();
+            dynamic data = JObject.Parse(res);
+            foreach (var key in _KeyMap)
+            {
+                if (data[iss + key] != null)
+                {
+                    data[key] = data[iss + key];
+                }
+            }
+            return data;
         }
+
     }
 
 }
